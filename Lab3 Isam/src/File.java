b7f5a8681c2b90c28d3b5826f7311fc12c962460
isamnagi <58230680+isamnagi@users.noreply.github.com>
2020-07-06 20:52:01 -0400

    Add files via upload

---
+++ Lab3 Isam/src/File.java
@@ -0,0 +1,22 @@
+
+public class File extends Document {
+	
+	//Data members
+		String pathName;
+		String content;
+		
+	//Constructors
+		public File() {
+			pathName = "";
+			content = "";
+		}
+		
+		//public File(String content) {
+		
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
